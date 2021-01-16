344ec667a3e90a08f91fb6b7737bbfba34fd640d
Tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-01-16 10:03:44 +0900

    [Add] tweet機能最小限

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/joho/godotenv"
+)
+
+func Env_load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("err loading ", err)
+	}
+}
+
+func main() {
+	Env_load()
+	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
+	api := anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"),os.Getenv("ACCESS_TOKEN_SECRET"))
+
+	text := "hello world"
+	t, err := api.PostTweet(text,nil)
+	if err != nil {
+		log.Fatal("err :",err)
+	}
+	fmt.Println(t.Text)
 }
